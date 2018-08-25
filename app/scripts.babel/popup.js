f111af8f04d35ac58604d253e35c5f3cfbd7715b
Robert James Gabriel <robert_gabriel@outlook.com>
2018-08-25 22:58:55 +0100

    Added popup

---
+++ app/scripts.babel/popup.js
@@ -0,0 +1,41 @@
+'use strict'
+
+var vm = new Vue({
+  el: '#app',
+  data: {
+    categories: [],
+    favorites: [],
+    errorMessage: '',
+    search: '',
+    loading: true,
+    paid: true,
+    error: true,
+
+  },
+
+  methods: {
+    // Update Licence
+    getZoomRatio: function () {
+      return document.documentElement.clientWidth / window.innerWidth;
+    },
+
+    getTrimmedSelection: function () {
+      const selection = String(window.getSelection());
+      return selection.replace(/^\s+|\s+$/g, '');
+    },
+    stripLinks: function (text) {
+      return text.replace(/<a[^>]*>([^<>]*)<\/a>/g, '$1');
+    },
+
+    load: function () {
+      this.loading = true; // Trigger the loading module
+
+    }
+  }
+})
+
+vm.load()
+
+// Set config settings
+Vue.config.productionTip = false;
+Vue.config.devtools = false;
