a2a4c788f591f36017c963d41f616db875116002
Robert James Gabriel <robert_gabriel@outlook.com>
2018-08-27 00:17:18 +0100

    Added effects

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -3,13 +3,19 @@
 var vm = new Vue({
   el: '#app',
   data: {
-    categories: [],
+    meanings: [],
     errorMessage: '',
     search: '',
     loading: true,
     paid: true,
     error: true,
 
+
+    term: '',
+    synonyms: [],
+    related: [],
+    ipa: [],
+    etymology: []
   },
 
   methods: {
@@ -28,7 +34,63 @@ var vm = new Vue({
 
     load: function () {
       this.loading = true; // Trigger the loading module
+    },
+    submit: function () {
+      console.log(this.search);
+      let query = this.search;
+      // Start loading frame data.
+      chrome.runtime.sendMessage({
+          method: 'lookup',
+          arg: query
+        },
+        response => {
+          if (response !== null || response !== '') {
+            console.log(response);
+            this.term = response.term ? response.term : '';
 
+
+
+            if (response.meanings) {
+              for (var i in response.meanings) {
+                const meaning = response.meanings[i];
+                let newObject = {
+                  content: this.stripLinks(meaning.content),
+                  type: meaning.type
+                }
+                this.meanings.push(newObject);
+              }
+            }
+
+
+
+
+            if (response.related) {
+              for (var i in response.related) {
+                var extern_link = `http://en.wikipedia.org/wiki/${response.related[i]}`;
+      
+                let newObject = {
+                  link: extern_link,
+                  title: response.related[i],
+                }
+      
+                this.related.push(newObject);
+                console.log(this.related);
+              }
+            }
+
+            if (response.etymology) {
+
+              this.etymology = response.etymology;
+
+            }
+
+            this.related = response.related ? response.related : '';
+            this.synonyms = response.synonyms ? response.synonyms : '';
+            this.ipa = response.ipa ? response.ipa : '';
+            
+          }
+        }
+      );
     }
   }
 })
